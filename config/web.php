f1be4977f60777b43a02489d723f2db4f0e645dc
Stu Carter <stu@practically.io>
2025-10-14 17:03:24 +0100

    Add REST API controller for users

--- config/web.php
+++ config/web.php
@@ -42,14 +42,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'users'],
+                '<controller>/<action>' => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
