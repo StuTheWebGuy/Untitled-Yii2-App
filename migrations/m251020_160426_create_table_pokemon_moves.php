855c2f5f7fc5d33af303bde78be1a21a817a69d3
Casper JB <casper_jb@icloud.com>
2025-10-20 21:09:44 +0100

    edited table: pokemon_moves - wrote migration

--- migrations/m251020_160426_create_table_pokemon_moves.php
+++ migrations/m251020_160426_create_table_pokemon_moves.php
@@ -2,6 +2,11 @@
 
 use yii\db\Migration;
 
+/**
+ * Handles the creation of table `{{%pokemon_moves}}`
+ *
+ * junction: links `{{%moves}}` to a `{{%pokemon}}`
+ */
 class m251020_160426_create_table_pokemon_moves extends Migration
 {
     /**
@@ -9,7 +14,37 @@ class m251020_160426_create_table_pokemon_moves extends Migration
      */
     public function safeUp()
     {
+        $this->createTable('{{%pokemon_moves}}', [
+            'move_id' => $this->integer()->notNull(),
+            'pokemon_instance_id' => $this->integer()->notNull(),
+            'PRIMARY KEY (move_id, pokemon_instance_id)',
+        ]);
 
+        /**
+         * effect_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_moves-move_id',
+            '{{%pokemon_moves}}',
+            'move_id',
+            '{{%moves}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        /**
+         * immunity_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_moves-pokemon_instance_id',
+            '{{%pokemon_moves}}',
+            'pokemon_instance_id',
+            '{{%pokemon_instances}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
     }
 
     /**
@@ -17,23 +52,8 @@ class m251020_160426_create_table_pokemon_moves extends Migration
      */
     public function safeDown()
     {
-        echo "m251020_160426_create_table_pokemon_moves cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('{{%pokemon_moves}}');
+        $this->dropForeignKey('fk-pokemon_moves-move_id', '{{%pokemon_moves}}');
+        $this->dropForeignKey('fk-pokemon_moves-pokemon_instance_id', '{{%pokemon_moves}}');
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m251020_160426_create_table_pokemon_moves cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
