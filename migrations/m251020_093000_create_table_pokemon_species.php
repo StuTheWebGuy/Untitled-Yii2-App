420db29942a816d01d99d84a0c238ca2da15d3a5
Casper JB <casper_jb@icloud.com>
2025-10-20 12:22:02 +0100

    created table: pokemon_species

---
+++ migrations/m251020_093000_create_table_pokemon_species.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+
+
+/**
+ * Handles the creation of table `{{%pokemon_species}}`
+ *
+ * stores all pokemon species from pokeAPI
+ *
+ * @note should not be written to during runtime - only preloaded with cronjob / manually
+ */
+class m251020_093000_create_table_pokemon_species extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%pokemon_species}}', [
+            'id' => $this->primaryKey(),
+            'images_collection_id' => $this->integer()->notNull(),
+
+            'name' => $this->string(255)->notNull(),
+            'description' => $this->text()->notNull(),
+        ]);
+
+        /**
+         * images_collection_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_species-images_collection_id',
+            '{{%pokemon_species}}',
+            'images_collection_id',
+            '{{%images_collections}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-pokemon_species-images_collection_id', '{{%pokemon_species}}');
+        $this->dropTable('{{%pokemon_species}}');
+    }
+}
