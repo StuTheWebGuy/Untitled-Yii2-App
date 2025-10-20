89a4883f9eaf48b4685f0c7e5ecf5f06414a05b3
Casper JB <casper_jb@icloud.com>
2025-10-20 15:53:40 +0100

    created table: effects

---
+++ migrations/m251020_141944_create_table_effects.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%effects}}`
+ *
+ * stores an enum lookup index for an effect
+ */
+class m251020_141944_create_table_effects extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%effects}}', [
+            'id' => $this->primaryKey(),
+
+            'name' => $this->string()->notNull(),
+            'effect_lookup' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%effects}}');
+    }
+}
