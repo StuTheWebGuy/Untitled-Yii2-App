7ea045f01e3b07431390aed02e9fbf9ffa4f08e8
Casper JB <casper_jb@icloud.com>
2025-10-20 12:22:12 +0100

    created table: pokemon_instances

---
+++ migrations/m251020_093403_create_table_pokemon_instances.php
@@ -0,0 +1,80 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%pokemon_instances}}`
+ *
+ * stores a pokemon instance with data about that species, and how it is specifically configured by the user
+ *
+ * @todo add validation for team_id XOR box_id
+ */
+class m251020_093403_create_table_pokemon_instances extends Migration
+
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->dropTable('{{%pokemon_instances}}');
+        $this->createTable('{{%pokemon_instances}}', [
+            'id' => $this->primaryKey(),
+            'team_id' => $this->integer(),
+            'box_id' => $this->integer(),
+            'pokemon_species_id' => $this->integer(),
+
+            'custom_name' => $this->string(255),
+        ]);
+
+        /**
+         * box_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_instances-box_id',
+            '{{%pokemon_instances}}',
+            'box_id',
+            '{{%boxes}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        /**
+         * team_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_instances-team_id',
+            '{{%pokemon_instances}}',
+            'team_id',
+            '{{%teams}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        /**
+         * pokemon_species_id FK
+         */
+        $this->addForeignKey(
+            'fk-pokemon_instances-pokemon_species_id',
+            '{{%pokemon_instances}}',
+            'pokemon_species_id',
+            '{{%pokemon_species}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-pokemon_instances-box_id', '{{%pokemon_instances}}');
+        $this->dropForeignKey('fk-pokemon_instances-team_id', '{{%pokemon_instances}}');
+        $this->dropForeignKey('fk-pokemon_instances-pokemon_species_id', '{{%pokemon_instances}}');
+        $this->dropTable('{{%teams}}');
+    }
+}
