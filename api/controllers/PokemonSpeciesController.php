89bcc357453207a4ba8172acb77ff867241c3399
Casper JB <casper_jb@icloud.com>
2025-11-04 14:00:21 +0000

    adding count controllers and loosening pagination rules for species controller

--- api/controllers/PokemonSpeciesController.php
+++ api/controllers/PokemonSpeciesController.php
@@ -3,9 +3,26 @@
 namespace app\controllers;
 
 use app\models\PokemonSpecies;
-use yii\rest\ActiveController;
+use app\rest\ActiveController;
+use yii\helpers\ArrayHelper;
 
 class PokemonSpeciesController extends ActiveController
 {
     public $modelClass = PokemonSpecies::class;
+
+    public function actions(): array
+    {
+        return ArrayHelper::merge(parent::actions(), [
+            'index' => [
+                'pagination' => [
+                    'pageSizeLimit' => [1,10000],
+                ]
+            ]
+        ]);
+    }
+
+    public function actionCount(): int
+    {
+        return $this->modelClass::find()->count();
+    }
 }
--- api/controllers/TeamsController.php
+++ api/controllers/TeamsController.php
@@ -9,4 +9,9 @@ use Yii;
 class TeamsController extends ActiveController
 {
     public $modelClass = Team::class;
+
+    public function actionUsersCount($userId): int
+    {
+        return $this->modelClass::find()->where(['user_id' => $userId])->count();
+    }
 }
