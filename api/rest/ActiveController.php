d93e9e12d2bf294fb03acbad3353abcf23af2d4a
Casper JB <casper_jb@icloud.com>
2025-11-05 10:45:23 +0000

    added custom controller docblocks

--- api/rest/ActiveController.php
+++ api/rest/ActiveController.php
@@ -4,8 +4,20 @@ namespace app\rest;
 
 use yii\filters\Cors;
 
+/**
+ * Class ActiveController
+ *
+ * Extends yii2's ActiveController class to add functionality.
+ *
+ * - Adds Cors handling
+ */
 class ActiveController extends \yii\rest\ActiveController
 {
+    /**
+     * @inheritdoc
+     *
+     * @return array List of behaviours being changed
+     */
     public function behaviors(): array
     {
         return ['cors' => ['class' => Cors::class]];
--- api/rest/Controller.php
+++ api/rest/Controller.php
@@ -4,8 +4,20 @@ namespace app\rest;
 
 use yii\filters\Cors;
 
+/**
+ * Class Controller
+ *
+ * Extends yii2's Controller class to add functionality.
+ *
+ * - Adds Cors handling
+ */
 class Controller extends \yii\rest\Controller
 {
+    /**
+     * @inheritdoc
+     *
+     * @return array List of behaviours being changed
+     */
     public function behaviors(): array
     {
         return ['cors' => ['class' => Cors::class]];
