e4249c0391c8d3aac4003e562bf8c7aa1668b1b9
Casper JB <casper_jb@icloud.com>
2025-11-05 09:31:19 +0000

    set controller serializer to return arrays

--- api/rest/ActiveController.php
+++ api/rest/ActiveController.php
@@ -3,9 +3,17 @@
 namespace app\rest;
 
 use yii\filters\Cors;
+use yii\rest\Serializer;
 
 class ActiveController extends \yii\rest\ActiveController
 {
+    /**
+     * @inheritdoc
+     */
+    public $serializer = [
+        'class' => Serializer::class,
+        'collectionEnvelope' => 'items',
+    ];
     public function behaviors(): array
     {
         return ['cors' => ['class' => Cors::class]];
--- api/rest/Controller.php
+++ api/rest/Controller.php
@@ -3,9 +3,17 @@
 namespace app\rest;
 
 use yii\filters\Cors;
+use yii\rest\Serializer;
 
 class Controller extends \yii\rest\Controller
 {
+    /**
+     * @inheritdoc
+     */
+    public $serializer = [
+        'class' => Serializer::class,
+        'collectionEnvelope' => 'items',
+    ];
     public function behaviors(): array
     {
         return ['cors' => ['class' => Cors::class]];
