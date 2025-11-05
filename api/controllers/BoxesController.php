78c07507c5cd5655ed4a6a8b07c8d22a9b4b7453
Casper JB <casper_jb@icloud.com>
2025-11-05 09:32:42 +0000

    added count endpoints to boxes and categories

--- api/controllers/BoxesController.php
+++ api/controllers/BoxesController.php
@@ -8,4 +8,9 @@ use app\rest\ActiveController;
 class BoxesController extends ActiveController
 {
     public $modelClass = Box::class;
+
+    public function actionUsersCount($userId): int
+    {
+        return $this->modelClass::find()->where(['user_id' => $userId])->count();
+    }
 }
--- api/controllers/CategoriesController.php
+++ api/controllers/CategoriesController.php
@@ -8,4 +8,9 @@ use app\rest\ActiveController;
 class CategoriesController extends ActiveController
 {
     public $modelClass = Category::class;
+
+    public function actionUsersCount($userId): int
+    {
+        return $this->modelClass::find()->where(['user_id' => $userId])->count();
+    }
 }
