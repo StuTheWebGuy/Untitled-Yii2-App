ae6d50e4daf41557245ee9a57a9b1eb4a448cc95
Casper JB <casper_jb@icloud.com>
2025-11-10 17:14:05 +0000

    re implemented cors behaviors to rest/

--- api/rest/ActiveController.php
+++ api/rest/ActiveController.php
@@ -2,9 +2,12 @@
 
 namespace app\rest;
 
+use yii\filters\ContentNegotiator;
 use yii\filters\Cors;
+use yii\filters\VerbFilter;
 use yii\helpers\ArrayHelper;
 use yii\rest\Serializer;
+use yii\web\Response;
 
 class ActiveController extends \yii\rest\ActiveController
 {
@@ -17,19 +20,32 @@ class ActiveController extends \yii\rest\ActiveController
     ];
     public function behaviors(): array
     {
-        return ArrayHelper::merge(parent::behaviors(), ['cors' =>
-            [
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::class,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+            ],
+            'cors' => [
                 'class' => Cors::class,
                 'cors' => [
                     'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['GET', 'HEAD', 'OPTIONS', 'DELETE', 'POST', 'PUT'],
                     'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
+                    // 'Access-Control-Allow-Credentials' => true,
                     'Access-Control-Max-Age' => 3600,
                     'Access-Control-Allow-Headers' => ['*'],
                     'Access-Control-Allow-Origin' => ['*'],
                 ]
-            ]
-        ]);
+            ],
+            // 'authenticator' => [
+            //     'class' => CompositeAuth::className(),
+            // ],
+            'verbFilter' => [
+                'class' => VerbFilter::class,
+                'actions' => $this->verbs(),
+            ],
+        ];
     }
 }
