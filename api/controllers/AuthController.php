195727beba9069c239c517f13619ceb8802271fb
Casper JB <casper_jb@icloud.com>
2025-11-11 16:09:22 +0000

    added auth controller with empty login and logout methods

---
+++ api/controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers;
+
+use app\rest\Controller;
+
+/**
+ * Controller for handling authentication operations (login, logout).
+ */
+class AuthController extends Controller
+{
+    /**
+     * Creates a new user session for the user, and logs them in to
+     * the application.
+     *
+     * @return void
+     */
+    public function actionLogin(): void
+    {
+
+    }
+
+    /**
+     * Destroys a user session, and logs the user out of the application.
+     *
+     * @return void
+     */
+    public function actionLogout(): void
+    {
+
+    }
+}
