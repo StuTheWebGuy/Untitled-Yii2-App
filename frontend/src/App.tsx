1070a566c586d5e63b7046e1c5f77dc4d40342ea
Casper JB <casper_jb@icloud.com>
2025-11-12 16:33:45 +0000

    added inital routing for categories and teams

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Test from './pages/Test.tsx'
-import Categories from './pages/Categories.tsx'
+import Categories from './pages/categories/Categories.tsx'
+import Category from './pages/categories/Category.tsx'
+import Teams from './pages/teams/Teams.tsx'
+import Team from './pages/teams/Team.tsx'
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Test" element={<Test />} />
         <Route path="/Categories" element={<Categories />} />
+        <Route path="/Category/:categoryId" element={<Category />} />
+        <Route path="/Category/:categoryId/Teams" element={<Teams />} />
+        <Route path="/Category/:categoryId/Team/:teamId" element={<Team />} />
       </Routes>
     </div>
   )
