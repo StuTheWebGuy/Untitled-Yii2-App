0101e618550e62688f877e6db79762f985df4e49
Casper JB <casper_jb@icloud.com>
2025-11-12 16:47:31 +0000

    overridden UsersController.php to use SignupForm for creating a new user

--- api/controllers/UsersController.php
+++ api/controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\forms\SignupForm;
 use app\rest\ActiveController;
 use app\models\User;
 
@@ -18,4 +19,13 @@ class UsersController extends ActiveController
      * @var string The model class associated with this controller.
      */
     public $modelClass = User::class;
+
+    public function actions(): array
+    {
+        $actions = parent::actions();
+
+        $actions['create']['modelClass'] = SignupForm::class;
+
+        return $actions;
+    }
 }
