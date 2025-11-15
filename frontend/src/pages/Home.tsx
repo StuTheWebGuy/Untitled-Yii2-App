9a5ddd43853a0d4593509f52709981d2efb00236
Casper JB <casper_jb@icloud.com>
2025-11-15 03:09:13 +0000

    added login button to homepage for testing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <h1>Home Page</h1>
+  const BASE_URL = 'http://127.0.0.1:8000'
+  async function loginDev() {
+    await fetch(`${BASE_URL}/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'test',
+        password: 'test',
+      }),
+    })
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={loginDev}>login</button>
+    </div>
+  )
 }
