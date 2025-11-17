0347b56554857f25772a0d2bcad9fc7f3b53d755
Casper JB <casper_jb@icloud.com>
2025-11-17 18:05:52 +0000

    added teams index and teams count action

--- api/controllers/CategoriesController.php
+++ api/controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\Category;
+use app\models\Team;
 use app\rest\ActiveController;
 
 /**
@@ -18,6 +19,7 @@ class CategoriesController extends ActiveController
      * @var string The model class associated with this controller.
      */
     public $modelClass = Category::class;
+    public string $teamClass = Team::class;
 
     /**
      * Returns the total number of categories belonging to this user
@@ -25,8 +27,30 @@ class CategoriesController extends ActiveController
      * @param int $userId
      * @return int
      */
-    public function actionUsersCount(int $userId): int
+    public function actionUsersCount(int $userId): int // todo: move this to UsersController
     {
         return $this->modelClass::find()->where(['user_id' => $userId])->count();
+    }
+
+    /**
+     * Returns the total number of teams in this category
+     *
+     * @param int $categoryId
+     * @return int
+     */
+    public function actionTeamsCount(int $categoryId): int
+    {
+        return $this->teamClass::find()->where(['category_id' => $categoryId])->count();
+    }
+
+    /**
+     * Returns the teams in this category
+     *
+     * @param int $categoryId
+     * @return array
+     */
+    public function actionTeamsIndex(int $categoryId): array
+    {
+        return $this->teamClass::find()->where(['category_id' => $categoryId])->all();
     }
 }
