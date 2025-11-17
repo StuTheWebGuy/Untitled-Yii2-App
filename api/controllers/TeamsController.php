644d8e61ab5d27be198ef065c77dd0b85c78ad27
Casper JB <casper_jb@icloud.com>
2025-11-17 18:03:17 +0000

    added a pokemon-index action

--- api/controllers/TeamsController.php
+++ api/controllers/TeamsController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use app\models\PokemonInstance;
+use app\models\PokemonSpecies;
 use app\models\Team;
 use app\rest\ActiveController;
 use Yii;
@@ -20,6 +22,8 @@ class TeamsController extends ActiveController
      * @var string The model class associated with this controller.
      */
     public $modelClass = Team::class;
+    public string $pokemonInstanceClass = PokemonInstance::class;
+    public string $pokemonSpeciesClass = PokemonSpecies::class;
 
     /**
      * Returns the total number of teams belonging to a user
@@ -27,7 +31,7 @@ class TeamsController extends ActiveController
      * @param int $userId
      * @return int
      */
-    public function actionUsersCount(int $userId): int
+    public function actionUsersCount(int $userId): int // todo: move this to UsersController
     {
         return $this->modelClass::find()->where(['user_id' => $userId])->count();
     }
@@ -41,5 +45,18 @@ class TeamsController extends ActiveController
     public function actionCategoryView(int $categoryId): array
     {
         return $this->modelClass::find()->where(['category_id' => $categoryId])->all();
+    }
+
+    /**
+     * Returns the pokemon instances + species within a team
+     *
+     * @param int $teamId
+     * @return array
+     */
+    public function actionPokemonIndex(int $teamId): array // todo: fix me
+    {
+        $instances = $this->pokemonInstanceClass::find()->where(['team_id' => $teamId])->all();
+        $species = array_map(fn($instance) => $this->pokemonSpeciesClass::find()->where(['id' => $instance->pokemon_species_id])->one(), $instances);
+        return ['instances' => $instances, 'species' => $species];
     }
 }
